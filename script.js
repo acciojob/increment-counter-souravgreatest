d4bda80a83a0d49216c423f31424597d0a2af1e3
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-03 17:00:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let counter=0;
+let val=document.getElementbyId('para');
+let but=document.getElementbyId('butt');
+
+function solve(){
+	counter++;
+	val.textContent=counter;
+	alert(counter)
+}
+
+but.addEventListener('click',solve);
