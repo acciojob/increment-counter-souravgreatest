39e0dd1b7b50ff037b60c6529f84ec5b93c2bc3b
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-03 17:21:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let val=document.getElementById('counter');
 let but=document.querySelector('#incrementBtn');
 
 function solve(){
+	alert(counter)
 	counter++;
 	val.innerHTML=counter;
-	alert(counter)
 }
 
 but.addEventListener('click',solve);
